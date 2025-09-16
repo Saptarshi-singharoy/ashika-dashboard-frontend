bea35bef565407627c79d8daa728db74d66ce4be
Saptarshi Singha Roy <135221302+Saptarshi-singharoy@users.noreply.github.com>
2025-09-16 16:26:21 +0530

    ngchart added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { DonutChartComponent } from './donut-chart/donut-chart.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LineChartComponent,
+    DonutChartComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class SharedModule { }
